e7c68ad2ad12178a1a1751eeba38ea51444c6fc5
Joseph Stone <josephdstone0@gmail.com>
2020-02-25 16:37:11 -0700

    initial commit

---
+++ BackendForAssessment/Controllers/AccountsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AccountsCodingTest.Factories;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BackendForAssessment.Controllers
+{
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/accounts")]
+        public string GetAllAccounts()
+        {
+            var accounts = AccountsMockDataFactory.CreateAccountsCollection();
+            return JsonConvert.SerializeObject(accounts);
+        }
+    }
+}
---
+++ BackendForAssessment/Models/AccountStatuses.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendForAssessment.Models
+{
+    public enum AccountStatuses
+    {
+        Active,
+        Inactive,
+        Overdue,
+    }
+}
